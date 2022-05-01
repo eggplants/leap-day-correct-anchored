d1c82f3389046e8cf106a3c999817a5a3c66bdc3
eggplants <w10776e8w@yahoo.co.jp>
2022-05-01 20:05:18 +0900

    fix: generate anchors correctly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,35 @@
-var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
+var sectionHeight = function () {
+  var total = $(window).height(),
+    $section = $("section").css("height", "auto");
 
   if ($section.outerHeight(true) < total) {
     var margin = $section.outerHeight(true) - $section.height();
     $section.height(total - margin - 20);
   } else {
-    $section.css('height','auto');
+    $section.css("height", "auto");
   }
-}
+};
 
 $(window).resize(sectionHeight);
 
-$(function() {
-  $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+$(function () {
+  $("section h1, section h2, section h3").each(function () {
+    $("nav ul").append(
+      "<li class='tag-" +
+        this.nodeName.toLowerCase() +
+        "'><a href='#" +
+        encodeURIComponent($(this).text().toLowerCase()) +
+        "'>" +
+        $(this).text() +
+        "</a></li>"
+    );
+    $(this).attr("id", encodeURIComponent($(this).text().toLowerCase()));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
-  $("nav ul li").on("click", "a", function(event) {
+  $("nav ul li").on("click", "a", function (event) {
     var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
+    $("html, body").animate({ scrollTop: position }, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
@@ -29,5 +37,5 @@ $(function() {
 
   sectionHeight();
 
-  $('img').on('load', sectionHeight);
+  $("img").on("load", sectionHeight);
 });
